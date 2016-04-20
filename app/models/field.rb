31c841a990904ac6b305a029bd112fb6d57fb285
amerov <maratamerov@gmail.com>
2016-04-20 23:24:05 +0500

    define Field methods

--- app/models/field.rb
+++ app/models/field.rb
@@ -1,3 +1,21 @@
 class Field
 
+  def initialize(width:, height:)
+    @with = width
+    @hieght = height
+  end
+
+  def alive?(x, y)
+
+  end
+
+  def set(x, y, v)
+
+  end
+
+  def items
+
+  end
+
 end
+
--- spec/models/field_spec.rb
+++ spec/models/field_spec.rb
@@ -3,10 +3,9 @@ require 'spec_helper'
 describe Field do
   let(:field) { Field.new(width: 10, height: 15) }
 
-  it 'should be represents a two-dimensional field of cells.' do
-    expect(field).to be respond_to :alive?
-    expect(field).to be respond_to :make
-    expect(field).to be respond_to :items
-    expect(field).to be respond_to :set
+  it 'should be respond_to' do
+    expect(field).to respond_to :alive?
+    expect(field).to respond_to :set
+    expect(field).to respond_to :items
   end
 end
