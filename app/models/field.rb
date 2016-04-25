c726e5800d33e725f59226064126caf35f203ee7
amerov <maratamerov@gmail.com>
2016-04-25 16:36:17 +0500

    implement Field#next; bug fixes

--- app/models/field.rb
+++ app/models/field.rb
@@ -9,22 +9,33 @@ class Field
   def alive?(x, y)
     _x = x + @width % @width
     _y = y + @height % @height
-    @items[_x][_y]
+    @items[_y][_x]
   end
 
   def set(x, y, v)
-    @items[x][y] = v
+    @items[y][x] = v
   end
 
   def items
     @items
   end
 
+  def next(x, y)
+    alive = 0
+    [-1, 0, 1].each do |i|
+      [-1, 0, 1].each do |j|
+        if (j != 0 || i != 0) && alive?(x + i, y + j)
+          alive += 1
+        end
+      end
+    end
+    alive == 3 || alive == 2 && alive?(x, y)
+  end
+
   private
 
   def set_items
-    @items = Array.new(@width, Array.new(@height))
+    @items = Array.new(@height) { Array.new(@width) { false } }
   end
 
 end
-
