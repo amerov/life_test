9d1de5697abf2e1c810180e2038bfe39494249a7
amerov <maratamerov@gmail.com>
2016-04-27 09:05:06 +0500

    Implement Life  init state

--- app/models/life.rb
+++ app/models/life.rb
@@ -4,9 +4,20 @@ class Life
   def initialize(width:, height:)
     @width = width
     @height = height
+    init_field
   end
 
   def step
 
+  end
+
+  private
+
+  def init_field
+    @prev = Field.new(width: @width, height: @height)
+    @next = Field.new(width: @width, height: @height)
+    (@width * @height / 4).times.each do
+      @prev.alive!(rand(@width), rand(@height))
+    end
   end
 end
