ec087feb67dfc3ff941c1d42988fed3019b40e1a
amerov <maratamerov@gmail.com>
2016-04-27 08:39:31 +0500

    Add Life init state spec

--- spec/models/life_spec.rb
+++ spec/models/life_spec.rb
@@ -8,6 +8,14 @@ describe Life do
     expect(life).to respond_to :next
   end
 
+  it 'should be a new Life game state with a random initial state' do
+    expect(life.prev).to be_a(Field)
+    expect(life.prev).to respond_to :items
+    expect(life.prev.items).to_not match_array(Life.new(width: 10, height: 15).prev.items)
+    expect(life.prev.items).to_not match_array(Life.new(width: 10, height: 15).prev.items)
+    expect(life.prev.items).to_not match_array(Life.new(width: 10, height: 15).prev.items)
+  end
+
   describe '#step' do
     it 'should be advances the game by one instant, recomputing and updating all cells' do
       expect(life).to respond_to :step
